2ac99187d4668db4749212ae96d6d2f709fccb28
Dominic <dominic.watts@hotmail.co.uk>
2019-05-18 00:10:38 +0100

    install

---
+++ app/code/Xigen/Address/Console/Command/Load.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Xigen\Address\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Load extends Command
+{
+    private $logger;
+    private $state;
+
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\App\State $state,
+        \Magento\Framework\Stdlib\DateTime\DateTime $dateTime,
+        \Magento\Customer\Model\AddressFactory $addressFactory
+    ) {
+        $this->logger = $logger;
+        $this->state = $state;
+        $this->dateTime = $dateTime;
+        $this->addressFactory = $addressFactory;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $this->input = $input;
+        $this->output = $output;
+        $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_GLOBAL);
+
+        $this->output->writeln('[' . $this->dateTime->gmtDate() . '] Start');
+
+        // $address = $this->addressFactory->create()->load('backoffice_id', 123456);
+        // $address = $this->addressFactory->create()->load(123456, 'backoffice_id');
+
+        
+        $address = $this->addressFactory->create()
+            ->getCollection()
+            ->addFieldToFilter('backoffice_id', array('eq' => 123456))
+            ->getFirstItem();
+        
+
+        if ($address && $address->getId()) {
+            $this->output->writeln('[' . $this->dateTime->gmtDate() . '] ' . $address->getCity());
+        }
+
+        $this->output->writeln('[' . $this->dateTime->gmtDate() . '] Finish');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName("xigen:address:load");
+        $this->setDescription("Load customer address by custom attribute");
+        parent::configure();
+    }
+}
---
+++ app/code/Xigen/Address/Setup/InstallData.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Xigen\Address\Setup;
+
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer_address');
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+
+        /** @var $attributeSet AttributeSet */
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+
+        $customerSetup->addAttribute('customer_address', 'backoffice_id', [
+            'type' => 'int',
+            'label' => 'Backoffice Id',
+            'input' => 'text',
+            'required' => false,
+            'visible' => true,
+            'user_defined' => true,
+            'sort_order' => 50,
+            'position' => 50,
+            'system' => 0,
+        ]);
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer_address', 'backoffice_id')
+            ->addData([
+                'attribute_set_id' => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms' => [
+                    'adminhtml_customer_address',
+                    'customer_address_edit',
+                    'customer_register_address',
+                ],
+            ]);
+
+        $attribute->save();
+    }
+}
